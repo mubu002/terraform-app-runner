e81c7a2eaa2928188d6940f6ec30a35a4f0d11e6
mubu002 <72385887+mubu002@users.noreply.github.com>
2025-11-19 19:37:05 +0000

    Endret actions fil

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "pgr301-2024-terraform-state"
-    key    = "<STUDENT-NAVN>/apprunner-a-new-state.state"
+    key    = "<mubu002>/apprunner-a-new-state.state"
     region = "eu-west-1"
   }
 }
